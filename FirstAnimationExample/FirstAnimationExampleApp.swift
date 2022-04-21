345160b14a9bf3d9a18da668ef09a5246cada734
Franka Keil <fkeil@lcs.on.ca>
2022-04-21 11:21:24 -0400

    Initial Commit

---
+++ FirstAnimationExample/FirstAnimationExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstAnimationExampleApp.swift
+//  FirstAnimationExample
+//
+//  Created by Franka Keil on 2022-04-21.
+//
+
+import SwiftUI
+
+@main
+struct FirstAnimationExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
